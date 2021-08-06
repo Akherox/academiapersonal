15ab2174ffd73ea2eee78384d15be9db324c3dd6
Akherox <bryanalexbm@gmail.com>
2021-08-05 21:46:06 -0300

    Botao Usuario Atualizado

--- src/pages/PerfilUsuario.vue
+++ src/pages/PerfilUsuario.vue
@@ -4,7 +4,36 @@
           <q-toolbar>
         <q-btn flat round dense icon="arrow_back_ios_new" to="/home" />
         <q-toolbar-title></q-toolbar-title>
-        <q-btn flat round dense icon="fas fa-user-circle" to="/perfilusuario" />
+        <q-btn flat round dense icon="settings" to="/perfilusuario" >
+      <q-menu>
+        <div class="row no-wrap q-pa-md">
+          <div class="column">
+            <div class="text-h6 q-mb-md">Settings</div>
+            <q-toggle v-model="mobileData" label="Use Mobile Data" />
+            <q-toggle v-model="bluetooth" label="Bluetooth" />
+          </div>
+
+          <q-separator vertical inset class="q-mx-lg" />
+
+          <div class="column items-center">
+            <q-avatar size="72px">
+              <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+            </q-avatar>
+
+            <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+            <q-btn
+              color="primary"
+              label="Logout"
+              push
+              size="sm"
+              v-close-popup
+            />
+          </div>
+        </div>
+      </q-menu>
+
+        </q-btn>
       </q-toolbar>
         </q-header>
         <h3>Perfil</h3>
@@ -19,7 +48,14 @@
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-  // name: 'PageName',
+  setup () {
+    return {
+      mobileData: ref(true),
+      bluetooth: ref(false)
+    }
+  }
 }
 </script>
