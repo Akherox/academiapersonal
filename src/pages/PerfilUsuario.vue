5d122d173a3ce05126ee1db0d7faddea91bec868
Jefferson Prado <jlfprado@hotmail.com>
2021-11-26 21:49:23 -0300

    atualização tela perfil

--- src/pages/PerfilUsuario.vue
+++ src/pages/PerfilUsuario.vue
@@ -37,7 +37,6 @@
       </q-toolbar>
     </q-header>
 
-
     <h3>Perfil</h3>
     <q-item>
       <q-item-section>
@@ -46,8 +45,101 @@
         /></q-avatar>
       </q-item-section>
     </q-item>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-item>
+        <q-item-section>
+          <q-input
+            filled
+            v-model="name"
+            label="Seu nome"
+            hint="Digite seu nome"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Campo obrigatorio']"
+          />
+        </q-item-section>
+      </q-item>
+      <q-separator spaced inset />
 
-    <q-item>
+      <q-item>
+        <q-item-section>
+          <q-input
+            filled
+            v-model="Sobrenome"
+            label="Seu Sobrenome"
+            hint="Digite seu Sobrenome"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Campo obrigatorio']"
+          />
+        </q-item-section>
+      </q-item>
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-input
+            class="col"
+            label="Data de nascimento*"
+            dark
+            v-model="date"
+            filled
+            type="date"
+            stack-label
+            hint="Data de nascimento"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Campo obrigatorio']"
+          />
+        </q-item-section>
+      </q-item>
+      <q-separator spaced inset />
+          
+
+      <q-item>
+        <q-item-section>
+          <q-input
+            filled
+            v-model="sexo"
+            label="Seu sexo"
+            hint="Digite sexo masculino ou feminino"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Campo obrigatorio']"
+          />
+        </q-item-section>
+      </q-item>
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-avatar rounded size="80px">
+            <img
+              src="https://www.treinus.com.br/blog/wp-content/uploads/2020/05/tipos-de-exercicios-f%C3%ADsicos.jpg"
+            />
+          </q-avatar>
+          <q-input
+            filled
+            v-model="objetivo"
+            label="Seu Objetivo"
+            hint="Digite seu Objetivo Ex: Hipertrofia muscular"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || 'Campo obrigatorio']"
+          />
+        </q-item-section>
+      </q-item>
+
+      <q-toggle v-model="accept" label="Eu aceito os termos" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
+
+    <!-- <q-item>
       <q-item-section>
         <q-item-label>NOME</q-item-label>
         <q-item-label caption lines="2">Willian Roberto Guimarães</q-item-label>
@@ -106,36 +198,70 @@
         stack-label
       />
     </q-item-section>
-    <q-separator spaced inset />
-    <q-item>
-      <q-item-section>
-        <q-item-label>FRASE INSPIRADORA E MOTIVACIONAL</q-item-label>
-        <q-item-label caption lines="2">No pain, no gain </q-item-label>
-      </q-item-section>
-    </q-item>
+    <q-separator spaced inset /> -->
+
     <q-footer>
       <a-menu />
-
     </q-footer>
   </q-page>
 </template>
 
+
+
 <script>
 import { ref } from "vue";
-import AMenu from 'components/AMenu';
+import AMenu from "components/AMenu";
+import { useQuasar } from "quasar";
 
 export default {
   components: {
-    AMenu
+    AMenu,
   },
   setup() {
+    const name = ref(null);
+    const date = ref(null);
+    const accept = ref(false);
+    const $q = useQuasar();
+
     return {
       mobileData: ref(true),
       bluetooth: ref(false),
-      date: ref(""),
+      
       model1: ref(null),
       time: ref(""),
-    };
+      name,
+      accept,
+      date,
+      Sobrenome: ref(""),
+      sexo: ref(""),
+      objetivo: ref(""),
+
+     onSubmit() {
+    if (accept.value !== true) {
+      $q.notify({
+        color: "red-5",
+        textColor: "white",
+        icon: "warning",
+        message: "You need to accept the license and terms first",
+      });
+    } else {
+      $q.notify({
+        color: "green-4",
+        textColor: "white",
+        icon: "cloud_done",
+        message: "Submitted",
+      });
+    }
   },
+  onReset() {
+    name.value = null;
+    date.value = null;
+    accept.value = false;
+  },
+
+    };
+
+  },
+  
 };
 </script>
